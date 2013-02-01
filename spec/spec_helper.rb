fe3630f7d46320252b58107ca98935ee0207bc82
B-Randon Newtron <ditoway@gmail.com>
2013-02-01 15:47:25 -0800

    Removed spec_helper that had nothing in it.

--- spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-module ContainerMacro
-  def create_example_tasks!(on)
-    on.create_task "Example Task", :normal
-    on.create_task "Another Task!", :low
-    on.create_task "Wow Another Task!"
-  end
-end
